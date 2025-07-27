f58bf6b3b0074c453436e5a8ce4ad86ff292895a
Nota0515 <businessbro78@gmail.com>
2025-07-27 19:58:55 +0530

    newwalala commit

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,6 +1,4 @@
-import { config } from "dotenv";
-
-// Always load default .env file (Vercel sets envs itself, you don't need to load files manually)
-config();
+import dotenv from "dotenv";
+dotenv.config();
 
 export const { PORT, DB_URI, NODE_ENV } = process.env;
